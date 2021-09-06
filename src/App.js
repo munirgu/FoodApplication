7342aa013c22399216ada4025b4145e592619136
munirgu <munirgu@maestralsolutions.com>
2021-09-06 09:55:13 +0200

    Adding Meals Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Fragment } from "react";
+import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+      <Meals/>
+      </main>
     </Fragment>
   );
 }
